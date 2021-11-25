c2a45c186a6be41081b319ae05e02ef6fd1c0afc
Ben Buchanan <bsbuchanan99@gmail.com>
2021-11-25 10:51:12 -0500

    Generate standard legal corp decks

--- classes/Deck.py
+++ classes/Deck.py
@@ -13,15 +13,45 @@ class Deck:
                 return
             self.current_influence += card['faction_cost']
 
-        count = 0 if card['stripped_title'] not in self.cards else self.cards[card['stripped_title']]
+        count = 0 if card['title'] not in self.cards else self.cards[card['title']]
         count += 1
-        self.cards[card['stripped_title']] = count
+        self.cards[card['title']] = count
         self.current_deck_size += 1
 
     def __str__(self):
-        id_string = self.identity['stripped_title']
+        id_string = self.identity['title']
         card_string = ""
         for card, count in self.cards.items():
             card_string += f"\n{count} {card}"
 
         return id_string + card_string
+
+class CorpDeck(Deck):
+    def __init__(self, identity):
+        super().__init__(identity)
+        self.agenda_point_range = self.generateAgendaPointRange()
+        self.current_agenda_points = 0
+
+    def generateAgendaPointRange(self):
+        base_minimum = 40
+        base_range = [18, 19]
+
+        size_diff = self.min_deck_size - base_minimum
+        num_ranges_up = size_diff // 5
+        range_increase = 2 * num_ranges_up
+        new_range = list(map(lambda r: r + range_increase, base_range))
+
+        return new_range
+
+    def addAgenda(self, card):
+        if card['faction_cost'] + self.current_influence > self.influence_limit:
+            return
+        if card['agenda_points'] + self.current_agenda_points > self.agenda_point_range[1]:
+            return
+        self.current_influence += card['faction_cost']
+        self.current_agenda_points += card['agenda_points']
+
+        count = 0 if card['stripped_title'] not in self.cards else self.cards[card['stripped_title']]
+        count += 1
+        self.cards[card['stripped_title']] = count
+        self.current_deck_size += 1
--- modules/deckbuilder.py
+++ modules/deckbuilder.py
@@ -1,6 +1,6 @@
 import random
 
-from classes.Deck import Deck
+from classes.Deck import Deck, CorpDeck
 
 def generate_runner_deck(card_pool):
     # Select an identity
@@ -23,4 +23,34 @@ def generate_runner_deck(card_pool):
     return deck
 
 def generate_corp_deck(card_pool):
-    return "Corp dekcs not yet implemented"
+    # Select an identity
+    identities = [ i for i in card_pool if i['type_code'] == 'identity' ]
+    random.shuffle(identities)
+    identity = identities.pop()
+
+    deck = CorpDeck(identity)
+
+    # Remove agendas and add those to the deck before anything else
+    agendas = [ a for a in card_pool if a['type_code'] == 'agenda'
+                                     and a['faction_code'] in [identity['faction_code'], 'neutral-corp'] ]
+
+    # Add agendas until we are in our point range
+    point_range = deck.agenda_point_range
+    while deck.current_agenda_points < point_range[0]:
+        random.shuffle(agendas)
+        agenda = agendas.pop()
+        number_to_add = random.randint(1, agenda['deck_limit'])
+        for i in range(number_to_add):
+            deck.addAgenda(agenda)
+
+    other_cards = [ c for c in card_pool if c['type_code'] not in ['identity', 'agenda'] ]
+
+    # Keep adding cards as long as minimum size hasn't been reached
+    while deck.current_deck_size < deck.min_deck_size:
+        random.shuffle(other_cards)
+        card = other_cards.pop()
+        number_to_add = random.randint(1, card['deck_limit'])
+        for i in range(number_to_add):
+            deck.addCard(card, card['faction_code'] == deck.identity['faction_code'])
+
+    return deck
